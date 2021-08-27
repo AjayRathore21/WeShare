9903e7e4a09e6871663e8ea64f16d23e23274c32
AjayRathore21 <ajaykumar420.ak79@gmail.com>
2021-08-27 16:22:57 +0530

    Done till Distributing Views and everything is working fine

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,26 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
+    User.findById(req.params.id,function(err,user){
+        if(err){return console.log('error in finding the user');}
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        })
     })
+    
+}
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            if(err){return console.log('error in updating the user');}
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('unauthorized');
+    }
+
 }
 
 // render the signup page
