61b807752bb242e4896b9d8ae72017dbee9ebd33
AjayRathore21 <ajaykumar420.ak79@gmail.com>
2021-08-27 14:22:41 +0530

    All done till delete and update post or comment

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+
 
 module.exports.create = function(req,res){
     console.log("inside the post create function", req.body);
@@ -13,3 +15,38 @@ module.exports.create = function(req,res){
         res.redirect('back');
     });
 }
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        if (post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+/*module.exports.destroy = function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        if(err){
+            console.log('error in finding the post')
+            return;
+        }
+        // .id converts the object id into string. use .id for comparison
+        if(post.user==req.user.id){
+            post.remove();
+
+            comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}*/
